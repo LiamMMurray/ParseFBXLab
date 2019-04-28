440135e110ef150b2b52930dd760dd7f86044884
Liam Murray <47370261+Liamade@users.noreply.github.com>
2019-04-28 05:14:07 -0400

    moves write_file function to member of singleton, adds data_in and data_out replacing a single data membert

--- prsfbx/FileUtil.h
+++ prsfbx/FileUtil.h
@@ -10,37 +10,36 @@ struct file_context
         static file_context* singleton;
 
        public:
-        std::string          data;
         static file_context* get() { return singleton; }
+        std::string          data_in;
+        std::string          data_out;
+
+        void write_file(std::string name)
+        {
+                std::fstream fstream;
+                fstream.open(name, std::ios_base::in | std::ios_base::ate);
+
+                if (fstream.tellp() > 0)
+                {
+                        std::cout << "this will overwrite an existing "
+                                     "file, continue? y/n\n";
+                        std::string answer;
+                        std::cin >> answer;
+                        if (answer == "n" || answer == "q")
+                                /*early exit*/
+                                return;
+                        else if (answer != "y")
+                                /*non-valid answer*/
+                                write_file(name);
+                }
+
+                fstream.close();
+                fstream.open(name, std::ios_base::out | std::ios_base::trunc);
+
+                fstream.write(file_context::get()->data_out.c_str(),
+                              file_context::get()->data_out.size());
+
+                fstream.close();
+        }
 };
 file_context* file_context::singleton = new file_context();
-
-void write_file(std::string name)
-{
-        std::fstream fstream;
-        fstream.open(name,
-                     std::ios_base::in | std::ios_base::ate);
-
-        if (fstream.tellp() > 0)
-        {
-                std::cout << "this will overwrite an existing "
-                             "file, continue? y/n\n";
-                std::string answer;
-                std::cin >> answer;
-                if (answer == "n" || answer == "q")
-                        /*early exit*/
-                        return;
-                else if (answer != "y")
-						/*non-valid answer*/
-                        write_file(name);
-        }
-
-        fstream.close();
-        fstream.open(name,
-                     std::ios_base::out | std::ios_base::trunc);
-
-        fstream.write(file_context::get()->data.c_str(),
-                      file_context::get()->data.size());
-
-        fstream.close();
-}
