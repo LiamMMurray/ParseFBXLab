1f2746a90c3f8c3bdc2474f3fb8e88e2afcffa86
Liam Murray <rodionskies@gmail.com>
2019-04-29 02:47:51 -0400

    adds fbx parsing singleton structure

--- prsfbx/FbxUtil.h
+++ prsfbx/FbxUtil.h
@@ -1,2 +1,12 @@
 #pragma once
 #include <fbxsdk.h>
+struct fbx_parse_context
+{
+      private:
+        fbx_parse_context(){};
+        static fbx_parse_context *singleton;
+
+      public:
+        static fbx_parse_context *get() { return singleton; }
+};
+fbx_parse_context *fbx_parse_context::singleton = new fbx_parse_context();
