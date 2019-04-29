63346f2d0d79ed21d3daf5bab65f620f5d08e8ea
Liam Murray <rodionskies@gmail.com>
2019-04-29 03:16:26 -0400

    adds unfinished compile_animation function to FbxUtil

--- prsfbx/FbxUtil.h
+++ prsfbx/FbxUtil.h
@@ -1,5 +1,7 @@
 #pragma once
+#include "Common/Common.h"
 #include <fbxsdk.h>
+#include <string>
 struct fbx_parse_context
 {
       private:
@@ -7,6 +9,33 @@ struct fbx_parse_context
         static fbx_parse_context *singleton;
 
       public:
+        FbxManager *              lSdkManager = NULL;
+        FbxScene *                lScene      = NULL;
         static fbx_parse_context *get() { return singleton; }
+        void                      compile_animation(std::string filepath)
+        {
+                InitializeSdkObjects(lSdkManager, lScene);
+                FbxString lFilePath(filepath.c_str());
+                if (lFilePath.IsEmpty())
+                        return;
+
+                FBXSDK_printf("\n\nFile: %s\n\n", lFilePath.Buffer());
+                bool lResult = LoadAnimationStacks(lSdkManager, lScene, lFilePath.Buffer());
+
+                if (lResult == false)
+                {
+                        FBXSDK_printf("\n\nAn error occurred while loading the scene...");
+                }
+                else
+                {
+                        if (!lScene)
+                        {
+                                FBX_ASSERT_NOW("null scene");
+                        }
+
+                        // get root node of the fbx scene
+                        FbxNode *lRootNode = lScene->GetRootNode();
+                }
+        }
 };
 fbx_parse_context *fbx_parse_context::singleton = new fbx_parse_context();
