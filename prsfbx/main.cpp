66a491b6619c7b51c1e977562388dea5517ad733
Liam Murray <lmmurray@student.fullsail.edu>
2019-04-29 04:18:29 -0400

    adds rename changes of command_line_context to main.cpp

--- prsfbx/main.cpp
+++ prsfbx/main.cpp
@@ -1,22 +1,22 @@
 #include "CommandLineUtil.h"
 #include "FileUtil.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
         if (argc == 1)
         {
-                command_line::get()->help();
+                command_line_context::get()->help();
                 /*early exit*/
                 return 0;
         }
 
         if (strcmp(argv[1], "help") == 0)
         {
-                command_line::get()->help(argc, argv);
+                command_line_context::get()->help(argc, argv);
         }
 
         if (strcmp(argv[1], "cmp") == 0)
         {
-                command_line::get()->cmp(argc, argv);
+                command_line_context::get()->cmp(argc, argv);
         }
 }
